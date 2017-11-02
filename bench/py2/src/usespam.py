60625257e1632a4b8f5c61b6e6d4fc914746ea78
Warren MacEvoy <wmacevoy@gmail.com>
2017-11-02 15:14:18 -0600

    python src

---
+++ bench/py2/src/usespam.py
@@ -0,0 +1,19 @@
+import sys
+import os
+
+# dynamically include local libraries
+thisdir=os.path.abspath(os.path.dirname(sys.argv[0]))
+libdir=thisdir + "/lib"
+
+for root, dirs, files in os.walk(libdir):
+    for dir in dirs:
+        if dir.startswith("lib."):
+            for subroot,subdirs,subfiles in os.walk(libdir + "/" + dir):
+                for subfile in subfiles:
+                    if (subfile.endswith(".so")):
+                        libsodir=libdir + "/" + dir
+                        sys.path.append(libsodir)
+
+import spam
+
+spam.system('echo ok')
