8bb209d6404d49d9523746d237d16163b45dc93d
Warren MacEvoy <wmacevoy@gmail.com>
2017-11-03 14:32:03 -0600

    gson dep in java

--- bench/rand/setup.py
+++
@@ -1,5 +0,0 @@
-from distutils.core import setup, Extension
- 
-ext = Extension('rand', sources=['randmodule.c'])
- 
-setup(name='rand', version='1.0', description='clib rand/srand', ext_modules=[ext])
